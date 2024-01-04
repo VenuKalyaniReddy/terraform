c01e5b82dd2dfe11dd2ed3db14eab10b4b3bf8c6
unknown <kalyaninalamaru.com>
2024-01-04 18:27:34 +0530

    Terraform scripts

---
+++ session_01/ec2.tf
@@ -0,0 +1,45 @@
+resource "aws_instance" "web" {
+  ami           = "ami-03265a0778a880afb" #devops-practice
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
+
+  tags = {
+    Name = "HelloTerraform"
+  }
+
+
+# aws_security_group
+
+resource  "aws_security_group" "roboshop-all" {
+    name = "roboshop-all-aws"
+    description = "allow TLS inbound traffic "
+
+}
+
+
+# inbound rule
+
+ingress  {
+    description = "Allow all ports"
+    from_port =0
+    to_port =0
+    protocol = "tcp"
+    cidr_block =["0.0.0.0/0"]
+}
+
+#outbound
+
+egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+tags = {
+    Name = " roboshop-all-aws"
+
+}
+
+}
